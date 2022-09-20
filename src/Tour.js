2c6a98b4dfc6dd7ab2c732da033426a3ba2936dc
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-09-20 15:55:37 +0500

    Tour info read more and read less feature

--- src/Tour.js
+++ src/Tour.js
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
 
 const Tour = ({ id, name, info, image, price }) => {
+  const [readMore, setReadMore] = useState(false);
+  const [btnName, setBtnName] = useState("read more");
+  const readmore = () => {
+    const read = readMore ? false : true;
+    const name = read ? "read less" : "read more";
+    setReadMore(read);
+    setBtnName(name);
+  };
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
       <footer>
         <div className="tour-info">
           <h4>{name}</h4>
-          <p>{info}</p>
+          <h4 className="tour-price">${price}</h4>
         </div>
+        <p>{readMore ? info : `${info.substring(0, 200)}...`}</p>
+        <button onClick={() => readmore()}>{btnName}</button>
+        <button className="delete-btn">not interested</button>
       </footer>
     </article>
   );
