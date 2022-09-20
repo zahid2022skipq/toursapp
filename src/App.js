deb55e43ab332f32278347821b39a52fc6921258
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-09-20 17:39:48 +0500

    Completed Tours App

--- src/App.js
+++ src/App.js
@@ -31,15 +31,34 @@ function App() {
     fetchTours();
   }, []);
 
-  return loading ? (
-    <main>
-      <Loading />
-    </main>
-  ) : (
-    <main>
-      <Tours tours={tours} removeTour={removeTour} />
-    </main>
-  );
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  if (!loading) {
+    return (
+      <main>
+        <Tours tours={tours} removeTour={removeTour} />
+      </main>
+    );
+  }
 }
 
 export default App;
